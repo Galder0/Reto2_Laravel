8a2604439899c14d17b75255604611e0e9cf88a0
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 14:50:33 +0000

    Role apiController and routes implemented

---
+++ app/Http/Controllers/API/RoleController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::all();
+
+        return response()->json(['roles' => $roles], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            // Add your validation rules for storing a role
+        ]);
+
+        $role = Role::create($request->all());
+
+        return response()->json(['role' => $role], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Role $role)
+    {
+        return response()->json(['role' => $role], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $request->validate([
+            // Add your validation rules for updating a role
+        ]);
+
+        $role->update($request->all());
+
+        return response()->json(['role' => $role], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+
+        return response()->json(['message' => 'Role deleted successfully'], 200);
+    }
+}
