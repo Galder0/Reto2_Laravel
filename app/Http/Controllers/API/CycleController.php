9da23fc8c93501fa04e5a8620ec935ace4423898
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 18:08:51 +0000

    Changes on Cycle and Department api controllers

--- app/Http/Controllers/API/CycleController.php
+++ app/Http/Controllers/API/CycleController.php
@@ -10,41 +10,72 @@ class CycleController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $cycles = Cycle::all();
+
+        return response()->json(['cycles' => $cycles], 200);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            // Add your validation rules for storing a cycle
+        ]);
+
+        $cycle = Cycle::create($request->all());
+
+        return response()->json(['cycle' => $cycle], 201);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Cycle  $cycle
+     * @return \Illuminate\Http\Response
      */
     public function show(Cycle $cycle)
     {
-        //
+        return response()->json(['cycle' => $cycle], 200);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cycle  $cycle
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Cycle $cycle)
     {
-        //
+        $request->validate([
+            // Add your validation rules for updating a cycle
+        ]);
+
+        $cycle->update($request->all());
+
+        return response()->json(['cycle' => $cycle], 200);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cycle  $cycle
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Cycle $cycle)
     {
-        //
+        $cycle->delete();
+
+        return response()->json(['message' => 'Cycle deleted successfully'], 200);
     }
 }
--- app/Http/Controllers/API/DepartmentController.php
+++ app/Http/Controllers/API/DepartmentController.php
@@ -5,46 +5,88 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DepartmentController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $departments = Department::all();
+        return response()->json($departments);
     }
+
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            // Add other validation rules for your fields
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $department = Department::create($request->all());
+
+        return response()->json($department, 201);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
      */
     public function show(Department $department)
     {
-        //
+        return response()->json($department);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Department $department)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            // Add other validation rules for your fields
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $department->update($request->all());
+
+        return response()->json($department, 200);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Department $department)
     {
-        //
+        $department->delete();
+
+        return response()->json(null, 204);
     }
 }
