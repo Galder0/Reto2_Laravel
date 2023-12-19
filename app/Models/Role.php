95749b0c2bb88b405b583d32db273bf033994ab8
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 14:24:45 +0000

    Models for relation between Role and User updated and working

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsToMany;
 
 class Role extends Model
 {
@@ -11,6 +12,6 @@ class Role extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class,'roles_users' ,'roles_id', 'user_id ');
     }
 }
