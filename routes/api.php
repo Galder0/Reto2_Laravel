8c092f3154fa536b42175446ef578c85285d2626
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 14:42:31 +0000

    CycleController and routes added to the Api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\DepartmentController;
 use App\Http\Controllers\API\ModuleController;
+use App\Http\Controllers\API\CycleController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -14,7 +15,10 @@ Route::apiResources([
     'department' => DepartmentController::class,
 ]);
 Route::apiResources([
-    'modules' => ModuleController::class,
+    'module' => ModuleController::class,
+]);
+Route::apiResources([
+    'cycle' => CycleController::class,
 ]);
 
 
@@ -35,3 +39,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
 });
 
 Route::resource('module', ModuleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('cycle', CycleController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
+    Route::post('cycle', [CycleController::class, 'store']); // Add the route for creating a department
+});
+
+Route::resource('cycle', CycleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
