b59573296829e2bb93f90d3480b3ef89ac47e509
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 14:57:15 +0000

    User Api controller and routes added

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+{
+    $users = User::paginate(20); // You can specify the number of items per page (e.g., 10)
+
+    return response()->json($users, 200);
+}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json(['user' => $user], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            // Add your validation rules for updating a user
+        ]);
+
+        $user->update($request->all());
+
+        return response()->json(['user' => $user], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,43 +6,64 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\DepartmentController;
 use App\Http\Controllers\API\ModuleController;
 use App\Http\Controllers\API\CycleController;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\UserController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
     return $request->user();
 });
 
 Route::apiResources([
-    'department' => DepartmentController::class,
+    'departments' => DepartmentController::class,
 ]);
 Route::apiResources([
-    'module' => ModuleController::class,
+    'modules' => ModuleController::class,
 ]);
 Route::apiResources([
-    'cycle' => CycleController::class,
+    'cycles' => CycleController::class,
 ]);
-
+Route::apiResources([
+    'roles' => RoleController::class,
+]);
+Route::apiResources([
+    'users' => UserController::class,
+]);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::resource('department', DepartmentController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
-    Route::post('department', [DepartmentController::class, 'store']); // Add the route for creating a department
+    Route::resource('departments', DepartmentController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
+    Route::post('departments', [DepartmentController::class, 'store']); // Add the route for creating a department
 });
 
-Route::resource('department', DepartmentController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
+Route::resource('departments', DepartmentController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::resource('module', ModuleController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
-    Route::post('module', [ModuleController::class, 'store']); // Add the route for creating a department
+    Route::resource('modules', ModuleController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
+    Route::post('modules', [ModuleController::class, 'store']); // Add the route for creating a department
 });
 
-Route::resource('module', ModuleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
+Route::resource('modules', ModuleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::resource('cycle', CycleController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
-    Route::post('cycle', [CycleController::class, 'store']); // Add the route for creating a department
+    Route::resource('cycles', CycleController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
+    Route::post('cycles', [CycleController::class, 'store']); // Add the route for creating a department
 });
 
-Route::resource('cycle', CycleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
+Route::resource('cycles', CycleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('roles', RoleController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
+    Route::post('roles', [RoleController::class, 'store']); // Add the route for creating a department
+});
+
+Route::resource('roles', RoleController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('users', UserController::class)->except(['index', 'show']); // Exclude 'index' and 'show' from the resource
+    Route::post('users', [UserController::class, 'store']); // Add the route for creating a department
+});
+
+Route::resource('users', UserController::class)->only(['index', 'show'])->withoutMiddleware(['auth:sanctum']);
