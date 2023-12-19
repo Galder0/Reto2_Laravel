1d304f266fd9a7499089d4701c1d06a710c21ae2
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 14:26:02 +0000

    Controllers for User and Role updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,22 +12,71 @@ class UserController extends Controller
     public function assignRolesForm(User $user)
     {
         $roles = Role::all();
-        $userRoles = $user->roles->pluck('id')->toArray();
+        $usersRoles = $user->roles; //->pluck('id')->toArray();
 
-        return view('users.assignRoles', compact('user', 'roles', 'userRoles'));
+        return view('users.assignRoles', compact('user', 'roles', 'usersRoles'));
     }
 
     public function assignRoles(Request $request, User $user)
+{
+    $request->validate([
+        'roles' => 'required|array',
+    ]);
+
+    DB::transaction(function () use ($request, $user) {
+        // Sync roles for the user
+        $user->roles()->sync($request->input('roles'));
+    });
+
+    return redirect('/admin')->with('success', 'Roles assigned successfully.');
+
+}
+
+    public function destroy(User $user)
+    {
+        DB::transaction(function () use ($user) {
+            // Detach roles before deleting the user
+            $user->roles()->detach();
+
+            // Delete the user
+            $user->delete();
+        });
+
+         $users = User::paginate(20);
+        return view('admin.index', compact('users'));
+    }
+    public function edit(User $user)
+    {
+        $roles = Role::all();
+        $userRoles = $user->roles;
+
+        return view('users.edit', compact('user', 'roles', 'userRoles'));
+    }
+
+    public function update(Request $request, User $user)
     {
         $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $user->id,
             'roles' => 'required|array',
         ]);
 
         DB::transaction(function () use ($request, $user) {
+            $user->update([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+            ]);
+
             // Sync roles for the user
             $user->roles()->sync($request->input('roles'));
         });
 
-        return redirect()->route('users.show', $user)->with('success', 'Roles assigned successfully.');
+        return redirect('/admin')->with('success', 'User updated successfully.');
     }
+    
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
 }
