8d5027b0c07799134b9ee0be9a6a2fd02e6d0d96
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-19 14:27:24 +0000

    seeders for user and roles automatic generation done

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'student']);
+        Role::create(['name' => 'staff']);
+        Role::create(['name' => 'teacher']);
+        // Add the 'student' role
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(1000)->create();
+        // Seed roles
+        $this->call(RoleSeeder::class);
+
+        // Get the 'student' and 'teacher' roles
+        $studentRole = Role::where('name', 'student')->first();
+        $teacherRole = Role::where('name', 'teacher')->first();
+
+        // Create 1000 users with the 'student' role
+        User::factory(1000)->create()->each(function ($user) use ($studentRole) {
+            $user->roles()->attach($studentRole);
+        });
+
+        // Create 80 users with the 'teacher' role
+        User::factory(80)->create()->each(function ($user) use ($teacherRole) {
+            $user->roles()->attach($teacherRole);
+        });
     }
 }
