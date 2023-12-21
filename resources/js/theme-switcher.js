e1f67010e15400db7b16f7364c08fbae235c385f
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-21 14:28:45 +0000

    Logos and theme switcher modified code

--- resources/js/theme-switcher.js
+++ resources/js/theme-switcher.js
@@ -1,46 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Obtener el tema guardado o usar el tema preferido por el sistema
-  const savedTheme = localStorage.getItem('theme');
-  const systemPreferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  const currentTheme = savedTheme || systemPreferredTheme;
+// Función para aplicar estilos específicos del tema al cuerpo de la página
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.display = "block";
+    document.body.classList.remove("preload");
+});
 
-  // Establecer el tema inicial
-  document.documentElement.setAttribute('data-bs-theme', currentTheme);
+function setTheme(themeValue) {
+    document.documentElement.setAttribute('data-bs-theme', themeValue);
+    localStorage.setItem('theme', themeValue);
 
-  // Función para alternar entre temas claro y oscuro
-  function toggleTheme(newTheme) {
-      // Actualizar el tema
-      document.documentElement.setAttribute('data-bs-theme', newTheme);
+    // Agregar o quitar la clase 'active' según el tema seleccionado
+    document.querySelectorAll('.dropdown-item').forEach(button => {
+        button.classList.remove('active');
+    });
+    document.querySelector(`[data-bs-theme-value="${themeValue}"]`).classList.add('active');
+}
 
-      // Guardar la configuración del tema en localStorage
-      localStorage.setItem('theme', newTheme);
+// Obtener el tema almacenado en localStorage o establecer 'light' como predeterminado
+const storedTheme = localStorage.getItem('theme') || 'light';
 
-      // Alternar la visibilidad del logo según el tema
-      const lightLogo = document.getElementById('lightLogo');
-      const darkLogo = document.getElementById('darkLogo');
+// Establecer el tema inicial y marcar como activo el botón correspondiente
+document.documentElement.setAttribute('data-bs-theme', storedTheme);
+document.querySelector(`[data-bs-theme-value="${storedTheme}"]`).classList.add('active');
 
-      lightLogo.style.display = newTheme === 'light' ? 'inline' : 'none';
-      darkLogo.style.display = newTheme === 'dark' ? 'inline' : 'none';
-  }
-
-  // Configuración inicial de visibilidad del logo
-  toggleTheme(currentTheme);
-
-  // Agregar un evento de clic a los botones de tema
-  document.querySelectorAll('.dropdown-item').forEach((button) => {
-      button.addEventListener('click', function () {
-          const newTheme = this.getAttribute('data-bs-theme-value');
-          toggleTheme(newTheme);
-
-          // Actualizar el atributo 'aria-pressed'
-          document.querySelectorAll('.dropdown-item').forEach((item) => {
-              item.classList.remove('active');
-          });
-          this.classList.add('active');
-      });
-  });
-
-  // Set the initial active state for the selected mode
-  const initialActiveButton = document.querySelector(`[data-bs-theme-value="${currentTheme}"]`);
-  initialActiveButton.classList.add('active');
+// Manejar clic en los botones de tema
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', (event) => {
+        const themeValue = event.currentTarget.getAttribute('data-bs-theme-value');
+        setTheme(themeValue);
+    });
 });
