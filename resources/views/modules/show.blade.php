d7e8664b0496e9de5688564076b119e6766b7324
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-12-21 19:21:11 +0000

    modules show-index views updated

--- resources/views/modules/show.blade.php
+++ resources/views/modules/show.blade.php
@@ -7,7 +7,26 @@
         <p><strong>ID:</strong> {{ $module->id }}</p>
         <p><strong>Name:</strong> {{ $module->name }}</p>
         <p><strong>Code:</strong> {{ $module->code }}</p>
-        <p><strong>Hours:</strong> {{ $module->numberhours }}</p>
+        <p><strong>Number of hours:</strong> {{ $module->numberhours }}</p>
+        <p><strong>Year:</strong> {{ $module->year }}</p>
+
+        <!-- Display cycles where the module is used -->
+        <div class="mb-4">
+            <h3>Used in Cycles:</h3>
+            @if ($module->cycles->isNotEmpty())
+                <ul>
+                    @foreach ($module->cycles as $cycle)
+                        <li>
+                            <a href="{{ route('cycles.show', $cycle) }}">
+                                {{ $cycle->name }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <p>This module is not used in any cycles.</p>
+            @endif
+        </div>
 
         <!-- Add more details as needed -->
 
