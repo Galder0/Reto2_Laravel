2571d78001dada301a7d6aa21ed28dfcb7df5aa8
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2024-01-10 20:11:38 +0000

    login controller updated to transfer from role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,33 +8,23 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated($request, $user)
+    {
+        // Check if the user has the 'admin' role
+        if ($user->hasRole('admin')) {
+            return redirect('/admin');
+        }
+
+        // Redirect to the default home page
+        return redirect($this->redirectTo);
     }
 }
