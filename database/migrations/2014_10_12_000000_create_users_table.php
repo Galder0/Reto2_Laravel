93d3b2a7a41628ac78cd8175fb3e237c8a7ecf92
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2024-01-11 19:25:01 +0000

    user table migrate fields updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,13 +20,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->softDeletes();
             $table->id();
             $table->string('name');
-            // $table->string('surnames')->nullable();
-            // $table->string('DNI')->nullable();
+            $table->string('surnames')->nullable();
+            $table->string('DNI')->nullable();
             $table->string('email')->unique();
-            // $table->string('direction')->nullable();
-            // $table->string('phone_number')->nullable();
+            $table->string('direction')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->boolean('fct_dual')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
